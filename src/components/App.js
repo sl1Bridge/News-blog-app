9b74a8858b53034bd5fece8ea06efd806cb3e9f3
Ilya Lukashevich <lukashevich.ilya000@gmail.com>
2020-01-04 20:47:55 +0300

    Edited MainHeaderComponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import '../styles/styles.css'
 import MainHeaderComponent from "./header/MainHeaderComponent";
+
 
 const App = () => (
   <div>
--- src/components/header/MainHeaderComponent.js
+++ src/components/header/MainHeaderComponent.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
-const MainHeaderComponent = () => (
-  <header>
-    Header
-  </header>
+import {withStyles} from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import HomeIcon from '@material-ui/icons/Home';
+
+const styles = {
+  headerContainer: {
+    height: '100px',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  homeButton: {
+    margin: '0 35px 0 15px',
+  },
+  homeButtonIcon: {
+    fontSize: '32px',
+  },
+  loginButton: {
+    fontSize: '18px',
+    marginRight: '15px',
+  },
+};
+
+const MainHeaderComponent = ({classes}) => (
+    <AppBar position="static">
+      <Toolbar className={classes.headerContainer}>
+        <IconButton edge="start"
+                    color="inherit"
+                    aria-label="home"
+                    className={classes.homeButton}
+        >
+          <HomeIcon className={classes.homeButtonIcon}/>
+        </IconButton>
+        <Typography variant="h4"
+                    className={classes.title}
+        >
+          News
+        </Typography>
+        <Button color="inherit"
+                className={classes.loginButton}
+        >
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
 );
 
-export default MainHeaderComponent;
+export default withStyles(styles)(MainHeaderComponent);
