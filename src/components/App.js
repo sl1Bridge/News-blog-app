f7f23e5c046dd7af361a56de658ed731b83cc5d7
Ilya Lukashevich <lukashevich.ilya000@gmail.com>
2020-01-09 13:28:23 +0300

    Hotfix

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 
 import MainHeaderComponent from "./header/MainHeaderComponent";
 import MainNewsListComponent from "./news/MainNewsListComponent";
