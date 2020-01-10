cf65f5575092c28f774555550fe376f18166b57f
Ilya Lukashevich <lukashevich.ilya000@gmail.com>
2020-01-10 04:21:00 +0300

    MainHeaderComponent: Added links to /, /login

--- src/components/header/MainHeaderComponent.js
+++ src/components/header/MainHeaderComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {withStyles} from "@material-ui/core";
+import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -25,28 +26,36 @@ const styles = {
     fontSize: '18px',
     marginRight: '15px',
   },
+  linkStyles: {
+    color: 'inherit',
+    textDecoration: 'inherit',
+  },
 };
 
 const MainHeaderComponent = ({classes}) => (
     <AppBar position="static">
       <Toolbar className={classes.headerContainer}>
-        <IconButton edge="start"
-                    color="inherit"
-                    aria-label="home"
-                    className={classes.homeButton}
-        >
-          <HomeIcon className={classes.homeButtonIcon}/>
-        </IconButton>
+        <Link to="/" className={classes.linkStyles}>
+          <IconButton edge="start"
+                      color="inherit"
+                      aria-label="home"
+                      className={classes.homeButton}
+          >
+            <HomeIcon className={classes.homeButtonIcon}/>
+          </IconButton>
+        </Link>
         <Typography variant="h4"
                     className={classes.title}
         >
           News
         </Typography>
-        <Button color="inherit"
-                className={classes.loginButton}
-        >
-          Login
-        </Button>
+        <Link to="/login" className={classes.linkStyles}>
+          <Button color="inherit"
+                  className={classes.loginButton}
+          >
+            Login
+          </Button>
+        </Link>
       </Toolbar>
     </AppBar>
 );
